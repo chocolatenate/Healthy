eb975c1a694fbc41c45013e6284f966ab48b58b3
chocolatenate <59070030@it.kmitl.ac.th>
2018-08-15 12:13:12 +0700

    first

---
+++ app/src/main/java/com/example/lab203_30/healthy/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.lab203_30.healthy;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.fragment_login);
+        setContentView(R.layout.fragment_register);
+    }
+}
